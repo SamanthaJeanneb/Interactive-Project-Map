3924f72ace1d4972ed97b6c183ae069e254e508d
Samantha J. Brown <123563481+SamanthaJeanneb@users.noreply.github.com>
2025-05-23 08:00:56 -0400

    Separate javascript

---
+++ ny_higher_ed_projects_map.js
@@ -0,0 +1,180 @@
+// ny_higher_ed_projects_map.js
+// JavaScript for Higher Ed Projects Map
+
+var map = L.map('mapid').setView([42.9, -75.0], 7);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://carto.com/attributions">CARTO</a> &copy; OpenStreetMap contributors'
+}).addTo(map);
+
+var maroonIcon = L.icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  shadowSize: [41, 41]
+});
+
+// Helper to generate clickable list items for popups
+function createPopupList(items, markerIdx) {
+  return '<ul>' + items.map(function(item, i) {
+    // Each li gets a data-marker and data-index for carousel
+    return `<li class="carousel-trigger" data-marker="${markerIdx}" data-imgidx="${i}" style="cursor:pointer;text-decoration:underline;color:#800000;">` +
+      `<b>${item.title}</b><br>${item.type}</li>`;
+  }).join('') + '</ul>';
+}
+
+// Data for each marker's projects (for carousel)
+var markerProjects = [
+  // 0 Cayuga Community College
+  [{ title: "Police Academy Renovation", type: "Academic Building" }],
+  // 1 Clarkson University
+  [{ title: "Locker Room Renovation", type: "Recreational Facility/Gymnasium" }],
+  // 2 Colgate University
+  [
+    { title: "Grounds Relocation Facility", type: "Maintenance Building" },
+    { title: "Frank Dining Hall Renovation", type: "Dining Facility" }
+  ],
+  // 3 Cornell University
+  [
+    { title: "Off Campus Housing Facilities", type: "Housing, Residence Hall" },
+    { title: "JOC Program Renovation", type: "Academic Building" },
+    { title: "Kimball Halls Restroom Renovation", type: "Academic Building" }
+  ],
+  // 4 SUNY Brockport
+  [{ title: "Brockport Residence Hall", type: "Housing, Residence Hall" }],
+  // 5 Jefferson Community College
+  [{ title: "Student Housing Facility", type: "Housing, Residence Hall" }],
+  // 6 SUNY Canton
+  [{ title: "Dana Hall Site Upgrades", type: "Housing, Residence Hall" }],
+  // 7 SUNY College of Environmental Science & Forestry
+  [
+    { title: "Bray Hall Renovation", type: "Academic Building" },
+    { title: "Post-Chlorination Facility Upgrade", type: "Maintenance Building" }
+  ],
+  // 8 SUNY Wanakena Campus Ranger Station
+  [{ title: "Ranger School Station Renovation", type: "nan" }],
+  // 9 St. Lawrence University
+  [
+    { title: "Campus Facility Assessment", type: "Academic Building" },
+    { title: "Theta Beta Pi Temple Restoration", type: "Academic Building" },
+    { title: "Java Barn Renovation", type: "Restaurants" },
+    { title: "Herring Briefing Room", type: "Administrative/Office Building" }
+  ],
+  // 10 SUNY Albany
+  [{ title: "Chemistry Lab Renovation", type: "Academic Building" }],
+  // 11 SUNY Cortland
+  [
+    { title: "Hendrick Hall Restroom Renovation", type: "Housing, Residence Hall" },
+    { title: "Bowers Hall Renovation", type: "Academic Building" }
+  ],
+  // 12 SUNY Oneonta
+  [{ title: "Lee Hall Stair and Retaining Wall", type: "Academic Building" }],
+  // 13 Syracuse University
+  [{ title: "Hawkins Warehouse Renovation", type: "Warehouse/Storage Building" }],
+  // 14 Tompkins Cortland Community College
+  [
+    { title: "Classroom Additions and Alterations", type: "Academic Building" },
+    { title: "Electrical System Upgrade", type: "Academic Building" },
+    { title: "Café Kitchen Renovation", type: "Dining Facility" }
+  ],
+  // 15 University of Rochester
+  [{ title: "Anderson Hall Renovation", type: "Housing, Residence Hall" }],
+  // 16 Wells College
+  [
+    { title: "Student Union and Athletic Renovation", type: "Recreational Facility/Gymnasium" },
+    { title: "Athletic Wing Renovation", type: "Recreational Facility/Gymnasium" }
+  ]
+];
+
+// Marker creation with clickable list items
+var markerIdx = 0;
+L.marker([42.9317, -76.5661], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>Cayuga Community College</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([44.6592, -74.9982], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>Clarkson University</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([42.817, -75.5385], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>Colgate University</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([42.4534, -76.4735], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>Cornell University</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([43.2130, -77.9500], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>SUNY Brockport</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([44.0007, -75.9223], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>Jefferson Community College</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([44.5913, -75.1626], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>SUNY Canton</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([43.0373, -76.1351], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>SUNY College of Environmental Science & Forestry</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([44.2224, -74.8919], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>SUNY Wanakena Campus Ranger Station</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([44.5902, -75.1642], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>St. Lawrence University</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([42.6864, -73.8238], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>State University of New York at Albany</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([42.6012, -76.1805], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>State University of New York at Cortland</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([42.457, -75.0638], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>State University of New York at Oneonta</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([43.0386, -76.134], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>Syracuse University</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([42.5287, -76.3406], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>Tompkins Cortland Community College</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([43.1293, -77.6283], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>University of Rochester</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+L.marker([42.7463, -76.6961], { icon: maroonIcon }).addTo(map)
+  .bindPopup(`<b>Wells College</b>${createPopupList(markerProjects[markerIdx], markerIdx++)}`);
+
+// Carousel logic
+var carouselModal = document.getElementById('carouselModal');
+var carouselImg = document.getElementById('carouselImg');
+var carouselCaption = document.getElementById('carouselCaption');
+var carouselCounter = document.getElementById('carouselCounter');
+var currentMarker = 0, currentImgIdx = 0;
+
+function showCarousel(markerIdx, imgIdx) {
+  currentMarker = markerIdx;
+  currentImgIdx = imgIdx;
+  var projects = markerProjects[markerIdx];
+  // Placeholder image for all
+  carouselImg.src = 'placeholder.png';
+  carouselCaption.textContent = projects[imgIdx].title + ' - ' + projects[imgIdx].type;
+  carouselCounter.textContent = (imgIdx + 1) + ' / ' + projects.length;
+  carouselModal.classList.remove('hidden');
+  document.body.style.overflow = 'hidden';
+}
+function hideCarousel() {
+  carouselModal.classList.add('hidden');
+  document.body.style.overflow = '';
+}
+function nextImg() {
+  var projects = markerProjects[currentMarker];
+  currentImgIdx = (currentImgIdx + 1) % projects.length;
+  showCarousel(currentMarker, currentImgIdx);
+}
+function prevImg() {
+  var projects = markerProjects[currentMarker];
+  currentImgIdx = (currentImgIdx - 1 + projects.length) % projects.length;
+  showCarousel(currentMarker, currentImgIdx);
+}
+
+document.getElementById('carouselCloseBtn').onclick = hideCarousel;
+document.getElementById('carouselNextBtn').onclick = nextImg;
+document.getElementById('carouselPrevBtn').onclick = prevImg;
+carouselModal.addEventListener('click', function(e) {
+  if (e.target === carouselModal) hideCarousel();
+});
+
+// Delegate click events for popup list items
+map.on('popupopen', function(e) {
+  var popup = e.popup;
+  setTimeout(function() { // Wait for DOM to render
+    var triggers = popup.getElement().querySelectorAll('.carousel-trigger');
+    triggers.forEach(function(li) {
+      li.onclick = function(ev) {
+        var markerIdx = parseInt(li.getAttribute('data-marker'));
+        var imgIdx = parseInt(li.getAttribute('data-imgidx'));
+        showCarousel(markerIdx, imgIdx);
+      };
+    });
+  }, 10);
+});
